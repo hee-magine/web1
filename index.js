db2133f75ab3a866df7d833a8ebf6456b6dff36a
hee-magine <78868009+hee-magine@users.noreply.github.com>
2021-02-11 01:22:40 +0900

    first version

---
+++ index.js
@@ -0,0 +1,32 @@
+const myBtn = document.getElementById('btn');
+function event1() {
+  alert('hi');
+}
+function event2(event) {
+  console.log(event);
+}
+
+myBtn.addEventListener('click',event1);
+myBtn.addEventListener('click',event2);
+const one = document.querySelector('#one');
+const fields = document.querySelectorAll('[data-field]');
+const task = {
+  title: '코드 에디터 개발',
+  manager: 'CastleRing, Raccoon Lee',
+  status: ''
+}
+
+for (let tag of fields) {
+  const field = tag.dataset.field;
+  tag.textContent = task[field]
+}
+
+const btns = document.querySelectorAll('.btn')
+for (let btn of btns) {
+  const status = btn.dataset.status;
+  btn.onclick = function () {
+    fields[2].textContent = status;
+    fields[2].dataset.status = status;
+  }
+}
+
